bfec583aad7ece21ba9673ed926d067714940cd6
Rustam Nuruzov <ru.nuruzov@gmail.com>
2018-08-10 18:38:49 +0600

    after change price

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 </head>
 <body>
 	
-
+<!-- oc_product и oc_product_description -->
 	<?php
-
+	error_reporting(E_ALL);
 			require_once "Classes/PHPExcel.php";
+			require_once "db.php";
 
-		$tmpfname = "clothes.xlsx";
+		$tmpfname = "size.xlsx";
 		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
 		$excelObj = $excelReader->load($tmpfname);
 		$worksheet = $excelObj->getActiveSheet();
@@ -27,7 +28,7 @@
 		for ($row=1; $row <=$lastRow ; $row++) {
 			
 
-			$myArr[$worksheet->getCell('B'.$row)->getValue()] = $worksheet->getCell('G'.$row)->getCalculatedValue();
+			$myArr[$worksheet->getCell('L'.$row)->getValue()] = $worksheet->getCell('G'.$row)->getCalculatedValue();
 
 			// getCell('B'.$row) указываем столбец и номер строки
 			//getCalculatedValue(); Возвращает высчитанную по формуле данную
@@ -49,6 +50,60 @@
 			$i++;
 		}
 		echo "</table>";
+
+
+
+
+		$sql = "
+			SELECT oc_product.sku, oc_product.price, oc_product.wholesale_price, oc_product_description.name
+			FROM oc_product
+			LEFT JOIN oc_product_description ON oc_product.product_id = oc_product_description.product_id
+		";
+
+		
+		$result = $con->query($sql);
+		$ok=0;
+		$flag=0;
+		$noChange = array();
+
+		$stmt = $con->prepare("UPDATE oc_product SET wholesale_price = ? WHERE sku = ?");
+
+		if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+		        
+		        foreach ($myArr as $key => $value) {
+		        	
+		        	if($key == $row['sku'] && $row['sku']!=''){
+		  				#$upd= "UPDATE oc_product SET wholesale_price = '.$value.' WHERE sku='{$row['sku']}'";
+		  				$stmt->bind_param('is', $value, $row['sku']);
+		  				$stmt->execute();
+        				
+		  				// if ($con->query($upd) === TRUE) {
+		  				//     echo "Record updated successfully";
+		  				// }else{
+		  				// 	#echo "error";
+		  				// }
+		        		$flag==1;
+		        	}else{
+		        		$flag=0;
+		        	}
+		        }
+
+		        if($flag==0){
+		        	$noChange[$ok] = $key;
+		        	$ok++;
+		        }
+		        else{
+		        	$flag=0;
+		        }
+		    }
+		} else {
+		    echo "0 results";
+		}
+		$stmt->close();
+
+		print_r($noChange) ;
 
 	?>
 
